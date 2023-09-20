23f585f1572f8b38a22b720461547e66e35e7bb3
Param <paramchhabra81@gmail.com>
2023-09-21 01:38:49 +0530

    ishupush

--- client/src/layouts/Login.svelte
+++ client/src/layouts/Login.svelte
@@ -1,44 +1,67 @@
 <script>
-  import TailwindCss from '../lib/TailwindCSS.svelte';
-
-  
+  import TailwindCss from "../lib/TailwindCSS.svelte";
 </script>
-
-<TailwindCss/>
-
-<main class="py-20">
-  <div class="logindiv py-10 px-16 rounded-lg">
-      <h1 class="">VITAMART</h1>
-      <p class="text-white">Feast. Share. Connect</p>
-      <img class="h-80 m-auto" src="../public/Logo.svg">
-      <button class="px-36 py-4 m-auto text-white">SignIn with Google</button>
-
-      
+  <TailwindCss/>
+<main>
+  <header class="flex bg-white justify-between fixed w-full">
+      <div class="mx-5"><button class="funt">VITAMART</button></div>
+      <div class="mx-5 my-auto">
+          <a href="#" class="px-7 text-2xl">Login</a>
+          <button class="rounded-xl started text-white px-5 py-3">Get Started</button>
+      </div>
+  </header>
+  <div class="h-16"></div>
+  <div class="bluediv bg-blue-300 flex flex-col  items-center">
+      <h1 class="text-white text-center py-20 text-8xl">Feast. Share. Connect</h1>
+      <p class="text-white text-center py-1 text-2xl mx-auto">Introducing VitaMart, your one-stop online marketplace tailored exclusively for the vibrant community within our college hostel.</p>
+      <button class="rounded-xl started text-white px-5 py-3 mt-2">Get Started</button>
   </div>
+
+  <div class="flex justify-between mx-5 py-5 gap-5">
+  <div class="text-left smoldiv text-white text-3xl">  
+    <h3>
+    Key Features:
+    <br>
+    <br>
+    1. Easy Registration: Signing up for VitaMart is a breeze. Use your college email to create an account and join the bustling virtual marketplace. 
+    <br>
+    2. Customized Listings: List your items with ease, adding detailed descriptions, high-quality photos, and your asking price.
+    <br>
+    3.Secure Messaging: Connect with buyers or sellers through our built-in messaging system.
+    <br>
+    4.Verified Profiles: Trust and transparency are essential.
+    <br>
+    <br>
+    Whether you're looking to satisfy your hunger pangs ,find affordable textbooks or get your DA's completed. VitaMart is the digital bazaar that connects buyers and sellers within the cozy confines of your campus accommodation.
+  </h3>
+  </div>
+  <div class="text-center smoldiv text-white text-3xl">
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa modi iure consectetur, cumque dicta impedit amet. Necessitatibus laboriosam earum ratione ipsa cumque ipsam quibusdam, perspiciatis libero, qui soluta molestias consequatur!</h3>
+  </div>
+</div>
+
 </main>
 
 <style>
+ @import url('https://fonts.googleapis.com/css2?family=Skranji&display=swap');
+ .smoldiv{
 
-  .logindiv {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-      box-shadow: 9px 10px 7px 1px rgba(38, 38, 38, 0.94);
-      background-color: rgb(46, 79, 79);
+ }
+  .funt{
+      padding-left: 1%;
+      font-family: "SKranji",cursive;
+    font-size: 50px;
+    color:rgb(193,226,0);
+  }
+  .started{
+      background-color: rgb(193,226,0);
   }
 
-  button:hover {
-      box-shadow: 9px 10px 7px 1px rgba(38, 38, 38, 0.54);
-      transition: box-shadow 0.25s ease-in-out;
+  p{
+      max-width: 500px;
   }
 
-  button {
-      background-color: rgb(193, 226, 0);
-  }
-
-  h1 {
-      font-size: 50px;
-      color: rgb(193, 226, 0);
+  .bluediv{
+      height: 700px;
   }
 </style>
